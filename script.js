b03415058399cd3f3c4eaea4c7f56e2a23c630ed
Allan Almeida <74943541+allansalmeida@users.noreply.github.com>
2025-02-11 10:31:27 -0300

    Arquivos adicionados ao github
    Arquivos adicionado para outros membros realizarem alterações

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito de mudança de cor na navbar ao rolar a página
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
